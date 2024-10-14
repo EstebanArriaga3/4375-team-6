3230f825f20db06d16caa2469df593b82cec974b
BrandeenoTW <bnguyen4096@gmail.com>
2024-10-14 00:03:49 -0500

    Initial python code for api and supporting files

---
+++ backend/Credentials.py
@@ -0,0 +1,5 @@
+class Creds:
+    address = 'cis4375group6.claogik2gtxd.us-east-2.rds.amazonaws.com'
+    username = 'admin'
+    password = 'Cis4375group6'
+    db = 'CIS4375Group6'
---
+++ backend/SQL.py
@@ -0,0 +1,36 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            password = user_password,
+            database = db_name
+        )
+        print('Connection to MySQL DB successful')
+    except Error as e:
+        print(f'The error "{e}" occurred')
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print('Query executed successfully')
+    except Error as e:
+        print(f'The error "{e}" occurred')
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor(dictionary=True)
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f'The error "{e}" occurred')
