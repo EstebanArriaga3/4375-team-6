abd302cdad5eb96d6385034ca483458995d682ca
dmoura100 <49538913+dmoura100@users.noreply.github.com>
2024-10-17 20:33:36 -0500

    Update Credentials.py
    needed to change db name

--- backend/Credentials.py
+++ backend/Credentials.py
@@ -2,4 +2,4 @@ class Creds:
     address = 'cis4375group6.claogik2gtxd.us-east-2.rds.amazonaws.com'
     username = 'admin'
     password = 'Cis4375group6'
-    db = 'CIS4375Group6'
+    db = 'TexasLawns'
